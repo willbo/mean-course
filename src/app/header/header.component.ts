bca6227c94199c588d7f4f57cef4152b87c351ff
Bill Kiernan <bill.kiernan@gmail.com>
2019-03-11 17:13:14 +0000

    finished adding auth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,6 +18,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
     this.authListenerSubs = this.authService
       .getAuthStatusListener()
       .subscribe(isAuthenticated => {
