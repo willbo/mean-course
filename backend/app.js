c942518def921268a3ce190009d555143d1b2c22
Bill Kiernan <bill.kiernan@gmail.com>
2019-03-12 16:06:25 +0000

    setup for frontend deploy

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb+srv://bill:' + process.env.MONGO_ATLAS_PW + '@cluster0
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join('images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
